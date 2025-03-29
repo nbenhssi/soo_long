76d26584c16acf58bbd5e68d9c21e60136ab2cbe
Noura <nourabenhssine10@gmail.com>
2025-03-29 12:56:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -29,3 +29,41 @@ int main()
     printf("%d\n", i);
     return 0;
 }
+
+
+
+static void	handle_path_error(char **map, t_mapcopy *cp, int count_c)
+{
+	if (cp->exit != 1)
+		write(2, "Error\nConnot exit\n", 19);
+	else
+		write(2, "Error\nConnot\n", 14);
+	ft_error(map);
+}
+
+int	main(int argc, char **argv)
+{
+	char		**map;
+	t_mapcopy	cp;
+	int			count_c;
+
+	map = NULL;
+	count_c = 0;
+	cp.exit = 0;
+	cp.cont_c = 0;
+	if (argc != 2 || !argv[1][0])
+		return (0);
+	map = read_map(argv[1]);
+	if (!map)
+		return (write(2, "Error\nMap not readed\n", 22), 1);
+	if (!check_file_is_valid(argv[1]) || !check_regtangle(map)
+		|| !check_element_doubles(map) || !check_walls(map))
+		ft_error(map);
+	cp.mapcopy = NULL;
+	copy_map(map, &cp);
+	flood_fill(&cp, cp.x_player, cp.y_player, &count_c);
+	free_map(cp.mapcopy);
+	if (cp.cont_c != count_c || cp.exit != 1)
+		handle_path_error(map, &cp, count_c);
+	return (libix(map), 0);
+}
